7e6893b9b18a897f87de90a5ac6fd471a467eb00
Collin Jilbert <cjilbert504@gmail.com>
2020-06-16 15:03:31 -0500

    Build out constructor and first fetch request

--- response-time-frontend/adapters/api_adapter.js
+++ response-time-frontend/adapters/api_adapter.js
@@ -1,3 +1,10 @@
 class ApiAdapter {
-    
+    constructor() {
+        this.baseUrl = "http://localhost:3000"
+    }
+
+    fetchPd(url) {
+        return fetch(`${this.baseUrl}/${url}`)
+        .then(response => response.json())
+    }
 }
