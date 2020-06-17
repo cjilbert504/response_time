fb574ead2e5d12f1c979217916eacf1fc2cd5cc1
Collin Jilbert <cjilbert504@gmail.com>
2020-06-17 17:07:24 -0500

    Add detail view method

--- response-time-frontend/components/police_dept.js
+++ response-time-frontend/components/police_dept.js
@@ -12,8 +12,33 @@ class PoliceDepartment {
     get renderName() {
         const stateDiv = document.getElementById(`${this.state}`);
         const p = document.createElement("p");
+
         p.setAttribute("data-id", this.id);
+        p.setAttribute("class", "departments");
         p.innerText = this.name;        
         return stateDiv.appendChild(p);
+    }
+
+    get detailView() {
+        const main = document.getElementById("main");
+        const ul = document.createElement("ul");
+        const li1 = document.createElement("li");
+        const li2 = document.createElement("li");
+        const li3 = document.createElement("li");
+        const li4 = document.createElement("li");
+        const li5 = document.createElement("li");
+        
+        li1.innerText = this.name;
+        li2.innerText = this.address;
+        li3.innerText = this.city;
+        li4.innerText = this.state;
+        li5.innerText = this.phone_number;
+
+        main.appendChild(li1);
+        main.appendChild(li2);
+        main.appendChild(li3);
+        main.appendChild(li4);
+        main.appendChild(li5);
+        return main;
     }
 }
