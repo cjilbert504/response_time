9309dfcc87af67890e6b5a2837bbe222b8c05c17
Collin Jilbert <cjilbert504@gmail.com>
2020-06-18 10:50:41 -0500

    Finish building constructor, add new instance method

--- response-time-frontend/components/review.js
+++ response-time-frontend/components/review.js
@@ -1,7 +1,13 @@
 class Review {
     constructor({id, commentor, comment}) {
         this.id = id
-        this.commentor
-        this.comment
+        this.commentor = commentor
+        this.comment = comment
+    }
+
+    get reviewDivMaker() {
+        const reviewPTag = document.createElement("p");
+        reviewPTag.innerText = `${this.commentor} says: ${this.comment}`
+        return reviewPTag;
     }
 }
