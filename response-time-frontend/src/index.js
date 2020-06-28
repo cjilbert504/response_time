0307d4f3d7f3287b3511f30c9afbda67ec76f4f1
Collin Jilbert <cjilbert504@gmail.com>
2020-06-27 21:57:44 -0500

    remove button in sidebar

--- response-time-frontend/src/index.js
+++ response-time-frontend/src/index.js
@@ -42,23 +42,23 @@ function expandSidebarBtns() {
 }
 
 
-function populateSidebar(statesArray) {                             // Try to break this into smaller functions and possibly refactor
+function populateSidebar(statesArray) {                             
     const div = document.getElementById("sidebar");
 
     statesArray.forEach(state => {
         const btn = document.createElement("button");
         const innerDiv = document.createElement("div");
-        const innerBtn = document.createElement("button");
+        // const innerBtn = document.createElement("button");
 
         btn.setAttribute("class", "accordion");
         innerDiv.setAttribute("class", "panel");
         innerDiv.setAttribute("id", state);
-        innerBtn.setAttribute("class", "accordionBtn");
+        // innerBtn.setAttribute("class", "accordionBtn");
 
         btn.innerText = state;
-        innerBtn.innerText = "Add a new police department";
+        // innerBtn.innerText = "Add a new police department";
 
-        innerDiv.appendChild(innerBtn);
+        // innerDiv.appendChild(innerBtn);
         div.appendChild(btn);
         div.appendChild(innerDiv);
         return div;
